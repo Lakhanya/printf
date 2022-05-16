770d87215a35a6bcbc269e80e90808b8315233ce
Lakhanya <lakhanya.mahlati@gmail.com>
2022-05-16 08:42:37 -0700

    Updated Header file

--- main.h
+++ main.h
@@ -2,10 +2,32 @@
 #define MAIN_H
 
 #include <stdarg.h>
+
 int _printf(const char *format, ...);
 int _putchar(char c);
 int _printf(const char *format, ...);
 int print_char(va_list arg);
 int print_str(va_list arg);
+int print_percent(void);
+void print_binary(unsigned int n, unsigned int* printed);
+int print_unsignedToBinary(va_list arg);
+int print_oct(va_list arg);
+int print_unsignedIntToHex(unsigned int num, char _case);
+int print_hex_base(va_list arg, char _case);
+int print_hex(va_list arg);
+int print_HEX(va_list arg);
+int print_STR (va_list arg);
+int print_int(va_list arg);
+
+/**
+ * identifierStruct - structure definition of a printTypeStruct
+ * @identifier: type
+ * @printer: function to print
+ */
+typedef struct identifierStruct
+{
+	char *identifier;
+	int (*printer)(va_list);
+} identifierStruct;
 
 #endif /*MAIN_H*/
